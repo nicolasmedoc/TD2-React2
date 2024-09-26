127f80129748931dd7ff3a7a58efe8332d42abd8
Nicolas Médoc <nicolas.medoc@list.lu>
2024-09-26 18:04:07 +0200

    commit init-redux branch

---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit'
+import matrixReducer from './redux/MatrixSlice'
+import configReducer from './redux/ConfigSlice'
+export default configureStore({
+  reducer: {
+    
+    }
+})
