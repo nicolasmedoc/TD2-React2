62ef00112be91bd16f42d21f5bfa4bc52aee2c26
Nicolas Médoc <nicolas.medoc@list.lu>
2024-10-01 17:25:58 +0200

    empty store.js

--- src/store.js
+++ src/store.js
@@ -1,6 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit'
-import matrixReducer from './redux/MatrixSlice'
-import configReducer from './redux/ConfigSlice'
 export default configureStore({
   reducer: {
     
